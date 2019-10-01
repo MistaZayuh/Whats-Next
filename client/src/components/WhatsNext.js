e287564608f52091c6c68c5fa72d544098b2470c
Ryan Pickett <piratematic@gmail.com>
2019-10-01 13:47:53 -0600

    fixed spacing

--- client/src/components/WhatsNext.js
+++ client/src/components/WhatsNext.js
@@ -1,31 +1,31 @@
 import React from "react";
 // import { Button, } from "semantic-ui-react";
-import { Header, Card,  } from "react-bootstrap";
+import { Header, Card, } from "react-bootstrap";
 import { Link, } from "react-router-dom";
 import building from "../images/building.jpeg"
 import styled from "styled-components";
 
 class WhatsNext extends React.Component {
 
-    render() {
-        return (
-            <div style={{display: "flex", justifyContent: "space-around"}}>
-                <Link>
-                <Card style={{ width: "600px", height: "250px" }} className="bg-dark text-white">
-                    <MyCardImage  src={building} alt="event location" />
-                    <Card.ImgOverlay>
-                        <Card.Title>THIS AWESOME EVENT</Card.Title>
-                        <Card.Text>
-                            #this section will be the description of the event.
+  render() {
+    return (
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <Link>
+          <Card style={{ width: "600px", height: "250px" }} className="bg-dark text-white">
+            <MyCardImage src={building} alt="event location" />
+            <Card.ImgOverlay>
+              <Card.Title>THIS AWESOME EVENT</Card.Title>
+              <Card.Text>
+                #this section will be the description of the event.
                          </Card.Text>
-                        <Card.Text>#eventDate</Card.Text>
-                    </Card.ImgOverlay>
+              <Card.Text>#eventDate</Card.Text>
+            </Card.ImgOverlay>
 
-                </Card>
-                </Link>
-            </div>
-        );
-    };
+          </Card>
+        </Link>
+      </div>
+    );
+  };
 };
 
 const MyCardImage = styled(Card.Img)`
