64d24d0ecaf26ca52d72bd0fb03b4d1151f4f4af
Ryan Pickett <54148241+Piratematic@users.noreply.github.com>
2019-10-24 16:26:13 -0600

    Revert "Animated page change"

--- client/src/App.js
+++ client/src/App.js
@@ -13,51 +13,29 @@ import Refresh from "./components/Refresh";
 import UserProfile from './components/UserProfile';
 import ProtectedRoute from './components/ProtectedRoute';
 import { Switch, Route, } from 'react-router-dom';
-import { Container, } from "semantic-ui-react";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
 import "../src/App.css";
-import styled from 'styled-components';
 
 const App = () => (
   <>
-    <div>
-      <NavBar />
-    </div>
+  <div>
+    <NavBar />
+  </div>
     <FetchUser>
-      <Route render={({ location }) => (
-        <PageContainer>
-          <TransitionGroup>
-            <CSSTransition
-              key={location.key}
-              timeout={300}
-              classNames='page'
-            >
-              <Switch location={location}>
-                <Route exact path="/" component={Explore} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/refresh" component={Refresh} />
-                <ProtectedRoute exact path="/dashboard" component={Home} />
-                <ProtectedRoute exact path="/events/new" component={EventForm} />
-                <Route exact path="/events/:id" component={EventView} />
-                <ProtectedRoute exact path="/events/:id/edit" component={EventForm} />
-                <ProtectedRoute exact path="/users/:id" component={UserProfile} />
-                <ProtectedRoute exact path="/users/:id/edit" component={UserForm} />
-                <Route component={NoMatch} />
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-        </PageContainer>
-      )}
-      />
+        <Switch>
+          <Route exact path="/" component={Explore} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/refresh" component={Refresh} />
+          <ProtectedRoute exact path="/dashboard" component={Home} />
+          <ProtectedRoute exact path="/events/new" component={EventForm} />
+          <Route exact path="/events/:id" component={EventView} />
+          <ProtectedRoute exact path="/events/:id/edit" component={EventForm} />
+          <ProtectedRoute exact path="/users/:id" component={UserProfile} />
+          <ProtectedRoute exact path="/users/:id/edit" component={UserForm} />
+          <Route component={NoMatch} />
+        </Switch>
     </FetchUser>
   </>
 )
-
-const PageContainer = styled.div`
-  position: relative;
-  width: 100vw;
-  height: 100vh;
-`;
 
 export default App;
--- client/src/components/Explore.js
+++ client/src/components/Explore.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import party from "../images/party.jpg";
-import styled, { keyframes, } from "styled-components";
+import styled from "styled-components";
 import Search from './Search';
 import Upcoming from "./Upcoming";
 import "../styles/Explore.css";
@@ -9,7 +9,6 @@ import { AuthConsumer } from "../providers/AuthProvider";
 import { Link, } from "react-router-dom";
 import { CardDeck, } from "react-bootstrap";
 import { Container, Button, Header, Segment, } from "semantic-ui-react";
-
 
 class Explore extends React.Component {
   state = { events: [], page: 0 };
@@ -28,47 +27,44 @@ class Explore extends React.Component {
     const { auth: { user } } = this.props
     if (this.state.page > 0) {
       var next = this.state.page - 1
-        axios.get(`/api/explore?page=${next}`)
-          .then(res => {
-            this.setState({ page: next, events: res.data })
-          })
+      axios.get(`/api/explore?specificuserid=${user.id}&page=${next}`)
+        .then(res => {
+          this.setState({ page: next, events: res.data })
+        })
     }
   };
 
   pageRight = () => {
     const { auth: { user } } = this.props
     var next = this.state.page + 1
-      axios.get(`/api/explore?page=${next}`)
-        .then(res => {
-          if (res.data.length > 0) {
-            this.setState({ page: next, events: res.data })
-          }
-        })
+    axios.get(`/api/explore?specificuserid=${user.id}&page=${next}`)
+      .then(res => {
+        if (res.data.length > 0) {
+          this.setState({ page: next, events: res.data })
+        }
+      })
   };
 
   render() {
     return (
-      <ExplorePageElm>
+      <div >
+        <div className="explore-btns">
+          <Button onClick={this.pageLeft} icon="left arrow"></Button>
+          <p className="pagination2">Page: {this.state.page + 1}</p>
+          <Button onClick={this.pageRight} icon="right arrow"></Button>
+        </div>
+        <div className="upcoming-outer">
 
-        <div >
-          <div className="explore-btns">
-            <Button onClick={this.pageLeft} icon="left arrow"></Button>
-            <p className="pagination2">Page: {this.state.page + 1}</p>
-            <Button onClick={this.pageRight} icon="right arrow"></Button>
-          </div>
-          <div className="upcoming-outer">
+          <div className="upcoming-explore">
+            <CardDeck>
 
-            <div className="upcoming-explore">
-              <CardDeck>
-
-                {this.state.events.map(e => (
-                  <Upcoming key={e.id} event={e} />
-                ))}
-              </CardDeck>
-            </div>
+              {this.state.events.map(e => (
+                <Upcoming key={e.id} event={e} />
+              ))}
+            </CardDeck>
           </div>
         </div>
-      </ExplorePageElm>
+      </div>
     )
   }
 };
@@ -81,41 +77,5 @@ const ConnectedExplore = (props) => (
   </AuthConsumer>
 );
 
-const slideInLeft = keyframes`
-  from {
-    -webkit-transform: translate3d(-100%, 0, 0);
-    transform: translate3d(-100%, 0, 0);
-    visibility: visible;
-  }
-
-  to {
-    -webkit-transform: translate3d(0, 0, 0);
-    transform: translate3d(0, 0, 0);
-  }
-`;
-
-const slideOutLeft = keyframes`
-  from {
-    -webkit-transform: translate3d(0, 0, 0);
-    transform: translate3d(0, 0, 0);
-  }
-
-  to {
-    visibility: hidden;
-    -webkit-transform: translate3d(-100%, 0, 0);
-    transform: translate3d(-100%, 0, 0);
-  }
-`;
-
-const Page = styled.div``;
-
-const ExplorePageElm = styled(Page)`
-  &.page-enter {
-    animation: ${slideInLeft} 0.2s forwards;
-  }
-  &.page-exit {
-    animation: ${slideOutLeft} 0.2s forwards;
-  }
-`;
 
 export default ConnectedExplore;
